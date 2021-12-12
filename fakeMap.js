5f8ab8906c3cd7efc6c085503cbce6c61f77ce0e
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-12 21:05:23 +0400

    fix: add fakeMap function

--- fakeMap.js
+++ fakeMap.js
@@ -2,8 +2,22 @@
 
 let nums = [2, 13, 24, 56, 89];
 
-nums.forEach(function (el, i) {
-  nums[i] = el * 2;
-});
+// nums.forEach(function (el, i) {
+//   nums[i] = el * 2;
+// });
 
-console.log(nums);
+// console.log(nums);
+
+let fakeMap = function (arr) {
+  let newArr = [];
+  let index = 0;
+  arr.forEach(function (el) {
+    newArr[index] = el * 2;
+    index += 1;
+  });
+
+  return newArr;
+};
+
+console.log(fakeMap(nums));
+console.log(nums); // nums remains unchanged
