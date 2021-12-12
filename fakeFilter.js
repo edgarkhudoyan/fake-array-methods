fc05d104731a34a4d9ab6fbeae825db845031ad5
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-12 20:56:32 +0400

    feat: add fakeFilter function

--- fakeFilter.js
+++ fakeFilter.js
@@ -1,1 +1,17 @@
 'use strict';
+let nums = [1, 3, 4, 6, 75, 22];
+
+let fakeFilter = function (arr) {
+  let newArr = [];
+  let index = 0;
+  arr.forEach(function (el) {
+    if (el % 2 === 0) {
+      newArr[index] = el;
+      index += 1;
+    }
+  });
+
+  return newArr;
+};
+
+console.log(fakeFilter(nums));
