c31946ad78ea5592fd1b5c09713739211c550f0b
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-12 21:06:08 +0400

    fix: remove commented code

--- fakeMap.js
+++ fakeMap.js
@@ -2,12 +2,6 @@
 
 let nums = [2, 13, 24, 56, 89];
 
-// nums.forEach(function (el, i) {
-//   nums[i] = el * 2;
-// });
-
-// console.log(nums);
-
 let fakeMap = function (arr) {
   let newArr = [];
   let index = 0;
