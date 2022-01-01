69a4d982f72f97b3bfda34db8ce7224dce8225bb
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-01-01 13:56:13 +0400

    add fakeReduce function

--- fakeReduce.js
+++ fakeReduce.js
@@ -1,20 +1,28 @@
 'use strict';
-const arr = [1, 3, 5, 7, 25];
+// const arr = [1, 3, 5, 7, 25];
 
-const fakeReduce = function (array, callback, initialValue) {
-  const hasInitial = initialValue !== undefined;
+// const fakeReduce = function (array, callback, initialValue) {
+//   const hasInitial = initialValue !== undefined;
 
-  const step = hasInitial ? 0 : 1;
-  let aggregator = hasInitial ? initialValue : array[0];
+//   const step = hasInitial ? 0 : 1;
+//   let aggregator = hasInitial ? initialValue : array[0];
 
-  array.forEach((el) => (aggregator = callback(aggregator, el, step, array)));
+//   array.forEach((el) => (aggregator = callback(aggregator, el, step, array)));
 
-  return aggregator;
-};
+//   return aggregator;
+// };
 
-console.log(
-  fakeReduce(arr, (acc, item, idx) => {
-    acc += item;
-    return acc;
-  })
-);
+// console.log(
+//   fakeReduce(arr, (acc, item, idx) => {
+//     acc += item;
+//     return acc;
+//   })
+// );
+
+function fakeReduce(array, combine, start) {
+  let current = start;
+  for (let element of array) {
+    current = combine(current, element);
+  }
+  return current;
+}
