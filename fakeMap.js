7f18197fe9ddb384201b63280f10ad6e7a08bd37
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-01-01 13:51:59 +0400

    add higher order function fakeMap

--- fakeMap.js
+++ fakeMap.js
@@ -1,17 +1,25 @@
 'use strict';
 
-let nums = [2, 13, 24, 56, 89];
+// let nums = [2, 13, 24, 56, 89];
 
-let fakeMap = function (arr) {
-  let newArr = [];
-  let index = 0;
-  arr.forEach(function (el) {
-    newArr[index] = el * 2;
-    index += 1;
-  });
+// let fakeMap = function (arr) {
+//   let newArr = [];
+//   let index = 0;
+//   arr.forEach(function (el) {
+//     newArr[index] = el * 2;
+//     index += 1;
+//   });
 
-  return newArr;
-};
+//   return newArr;
+// };
 
-console.log(fakeMap(nums));
-console.log(nums); // nums remains unchanged
+// console.log(fakeMap(nums));
+// console.log(nums); // nums remains unchanged
+
+function fakeMap(array, transform) {
+  let mapped = [];
+  for (let element of array) {
+    mapped.push(transform(element));
+  }
+  return mapped;
+}
